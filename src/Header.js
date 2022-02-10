95c91c3387bacb7b4ff7fa54698159ebb931ce3e
unknown <navjot1561.cse19@chitkara.edu.in>
2022-02-11 05:09:50 +0530

    lo bhai

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <div className="header">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRL3a-FlPpkxbx4iE0xg28b5SXgP49fxkR-hw&usqp=CAU" alt="logo" width="70" height="50" style={{borderRadius:"1rem"}}/>
+        <h1 style={{marginLeft:"1rem"}}>Google Keep</h1>
+      </div>
+    </>
+  );
+};
+
+export default Header;
